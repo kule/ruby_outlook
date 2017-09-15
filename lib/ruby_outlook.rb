32d43f7f0a641a4ecb8078eabff337ab8729b697
Luke Pearce <luke.pearce@worldprogramming.com>
2017-09-15 16:25:58 +0100

    add get folder and get folder by id apis

--- lib/ruby_outlook.rb
+++ lib/ruby_outlook.rb
@@ -419,6 +419,56 @@ module RubyOutlook
       JSON.parse(create_forward_response)
     end
 
+    # token (string): access token
+    # view_size (int): maximum number of results
+    # page (int): What page to fetch (multiple of view size)
+    # fields (array): An array of field names to include in results
+    # sort (hash): { sort_field: field_to_sort_on, sort_order: 'ASC' | 'DESC' }
+    # user (string): The user to make the call for. If nil, use the 'Me' constant.
+    # folder (string): The parent folder to return only folders within it.
+    def get_folders(token, view_size, page, fields = nil, sort = nil, user = nil, folder = nil)
+      request_url = "/api/v2.0/" << (user.nil? ? "Me" : ("users/" << user)) << "/MailFolders"
+
+      unless folder.blank?
+        request_url << "/#{folder}/childfolders"
+      end
+
+      request_params = {
+        '$top' => view_size,
+        '$skip' => (page - 1) * view_size
+      }
+
+      unless fields.nil?
+        request_params['$select'] = fields.join(',')
+      end
+
+      unless sort.nil?
+        request_params['$orderby'] = sort[:sort_field] + " " + sort[:sort_order]
+      end
+
+      get_folders_response = make_api_call "GET", request_url, access_token, request_params
+
+      JSON.parse(get_folders_response)
+    end
+
+    # token (string): access token
+    # id (string): The Id of the message to retrieve
+    # fields (array): An array of field names to include in results
+    # user (string): The user to make the call for. If nil, use the 'Me' constant.
+    def get_folder_by_id(token, id, fields = nil, user = nil)
+      request_url = "/api/v2.0/" << (user.nil? ? "Me" : ("users/" << user)) << "/MailFolders/#{id}"
+
+      request_params = nil
+
+      unless fields.nil?
+        request_params = { '$select' => fields.join(',') }
+      end
+
+      get_folder_response = make_api_call "GET", request_url, token, request_params
+
+      JSON.parse(get_folder_response)
+    end
+
     #----- End Mail API -----#
 
     #----- Begin Calendar API -----#
